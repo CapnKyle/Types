1e4553103eb23229b62be067c54b2c3b3edbb798
CapnKyle <kylewkelley@gmail.com>
2020-01-23 10:38:48 -0500

    Types
    Types

---
+++ Types/Types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Kyle";
+            int grade = 98;
+            char letterGrade = 'A';
+            double costOfTuition = 15000.00;
+
+            Console.WriteLine($"{firstName} is a student, {grade} is the grade they earned, which is an {letterGrade} and it cost {costOfTuition} to go to school");
+
+        }
+    }
+}
